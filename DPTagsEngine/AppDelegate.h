8ae26ea7774f84add39c533c75cf24cebdc75167
Kostas Antonopoulos <kostas@devpro.gr>
2012-11-04 23:15:38 +0200

    Initial Commit

---
+++ DPTagsEngine/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DPTagsEngine
+//
+//  Created by Kostas Antonopoulos on 11/4/12.
+//  Copyright (c) 2012 Kostas Antonopoulos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
